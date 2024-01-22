bd68f1f9f85614b9fdb615bc682d25f0d1fe37dc
Rodina <Rodinamobark3@gmail.com>
2024-01-22 20:23:14 +0200

    adding splash screen and little steps onboarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -41,6 +42,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // navigation
+    val nav_version = "2.7.5"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    //Lottie library
+    val lottie_version = "3.4.0"
+    implementation( "com.airbnb.android:lottie:$lottie_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/newweatherapp/SplashScreenFragment.kt
@@ -0,0 +1,28 @@
+package com.example.newweatherapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        lifecycleScope.launch {
+            delay(5000)
+            findNavController().navigate(R.id.detailedHourlyForecastFragment)
+        }
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+
+    }
+
+}
